3a50376c39b9bfbc22faee815f782ce50bdbeab4
Zanzibar <zanzisworld42@gmail.com>
2024-11-16 15:35:10 -0800

    Clean up confirmation page

--- pages/signin/confirm.vue
+++ pages/signin/confirm.vue
@@ -2,61 +2,37 @@
   <div class="confirm-page">
     <h1>Sign In Confirmation</h1>
     <div v-if="userStore.isSignedIn" class="w-3/4">
-      <p>Thanks for signing in.</p>
-
-      <p>
-        <NuxtLink v-if="!!returnTo" :to="returnTo">Continue from where you left off.</NuxtLink>
-      </p>
-      <div v-if="isLoading">
-        <p>Loading user info...</p>
-      </div>
-      <div v-else-if="error">
-        <p>Error loading user info: {{ error }}</p>
-      </div>
-      <div v-else>
-        <p>We shall call you {{ userStore.alias || 'The Amazing Wonder Person' }}</p>
-        <p>Your user ID is {{ userStore.id || 'unknown' }}</p>
-      </div>
-
-      <div v-if="userStore.isSignedIn">
-        <p>Now that you're signed in, you can:</p>
-        <ul>
-          <li>
-            <NuxtLink v-if="!!returnTo" :to="returnTo">Go back to where you were.</NuxtLink>
-            <NuxtLink to="/">Use Flux</NuxtLink>
-          </li>
-        </ul>
-      </div>
+      <div class="mb-4">Thanks for signing in. We are glad you are here.</div>
+      <div class="mb-4">If you are reading this, you may need to help things along.</div>
+      <div class="mb-4">If you just signed in for the first time, you can <NuxtLink to="/join">finish setting up your
+          profile
+        </NuxtLink>. Or if you already have a profile
+        (with a handle and display name), you can <NuxtLink to="/">use Flux</NuxtLink>.</div>
     </div>
     <p v-else>
-      You are not signed in. Please try signing in again. Or if you already have, maybe we broke something. Hmm, that
-      would be annoying.
+      Oh my. You are not signed in. Strange that you ended up here. Please <NuxtLink to="/join">try signing in
+      </NuxtLink>.
+      Or if you already tried, maybe we broke something. Hmm, that would be annoying.
     </p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
 import { useCookie } from 'nuxt/app'
 
 const userStore = useUserStore()
 const authService = useAuthService()
-
-const route = useRoute()
-const sessionToken = ref<string | null>(null)
 const returnTo = useCookie('return-to')
-
-const isLoading = computed(() => authService.loading.value)
-const error = computed(() => authService.error.value)
 
 onMounted(async () => {
   // expect cookie to have been sent by server and stored
   await authService.getCurrentUser()
-  sessionToken.value = route.query.token as string
 
   if (userStore.isSignedIn && returnTo.value) {
     navigateTo(returnTo.value)
     returnTo.value = ''
+  } else {
+    navigateTo('/')
   }
 })
 </script>
