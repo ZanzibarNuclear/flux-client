2a5b53568bada57857abe1df1a87ee8792e64fe9
Zanzibar <zanzisworld42@gmail.com>
2024-11-16 15:49:16 -0800

    Add about popup with copyright notice.

--- components/Layout/CompactFooter.vue
+++ components/Layout/CompactFooter.vue
@@ -1,23 +1,40 @@
 <template>
   <footer class="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200">
     <nav>
-      <div class="max-w-lg mx-auto px-4 py-3 flex justify-around">
+      <div class="max-w-xl mx-auto px-4 py-3 flex justify-around">
         <UButton to="/" icon="i-ph-arrow-bend-left-up-duotone" variant="ghost" color="blue" class="flex-1 align-middle"
           label="Reload" />
         <UButton to="/explore" icon="i-ph-magnifying-glass-duotone" variant="ghost" color="blue"
           class="flex-1 align-middle" label="Explore" />
         <UButton @click="goToProfileOrSignIn" icon="i-ph-user-circle-duotone" variant="ghost" color="blue"
           class="flex-1 align-middle" label="Profile" />
+        <UButton @click="aboutModalOpen = true" icon="i-ph-info-duotone" variant="ghost" color="blue"
+          class="flex-1 align-middle" label="About" />
         <UButton @click="goToWoN" icon="i-ph-globe-simple-duotone" variant="ghost" color="blue"
           class="flex-1 align-middle" label="World of Nuclear" />
       </div>
     </nav>
+    <UModal v-model="aboutModalOpen">
+      <UCard>
+        <template #header>About World of Nuclear</template>
+        <div>
+          <div class="mb-4">World of Nuclear is a platform for nuclear enthusiasts and professionals. It is a place to
+            learn, share, and discuss nuclear energy.</div>
+          <div class="text-sm text-gray-500">&copy; 2024 Nuclear Ambitions LLC. All rights reserved.</div>
+        </div>
+        <template #footer>
+          <UButton @click="aboutModalOpen = false" label="OK" />
+        </template>
+      </UCard>
+    </UModal>
   </footer>
 </template>
 
 <script lang="ts" setup>
 const userStore = useUserStore()
 const fluxStore = useFluxStore()
+
+const aboutModalOpen = ref(false)
 
 const goToWoN = () => {
   navigateTo('https://www.worldofnuclear.com', { external: true })
