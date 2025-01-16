6b5db28b877ee8887418d9577f19d29f12e165bd
Zanzibar <zanzisworld42@gmail.com>
2025-01-15 16:58:23 -0800

    Align some things

--- components/AccountWidget.vue
+++ components/AccountWidget.vue
@@ -1,36 +1,46 @@
 <template>
-  <UButton v-if="!userStore.isSignedIn" icon="i-ph-sign-in" label="Sign In" to="/join" color="gray" variant="solid" />
+  <UButton
+    v-if="!userStore.isSignedIn"
+    icon="i-ph-sign-in-duotone"
+    label="Sign In"
+    to="/join"
+    color="gray"
+    variant="solid" />
   <UDropdown v-else :popper="{ placement: 'bottom-start' }" :items="items">
-    <UButton color="primary" variant="solid" :icon="widgetIcon" :label="widgetLabel"
+    <UButton
+      color="primary"
+      variant="solid"
+      size="sm"
+      :icon="widgetIcon"
       trailing-icon="i-ph-caret-double-down" />
   </UDropdown>
 </template>
 
 <script lang="ts" setup>
-const userStore = useUserStore()
-const fluxStore = useFluxStore()
-const authService = useAuthService()
+const userStore = useUserStore();
+const fluxStore = useFluxStore();
+const authService = useAuthService();
 
-const widgetLabel = computed(() => userStore.alias || 'You Know Who')
-const widgetIcon = computed(() => 'i-ph-user-duotone')
+const widgetLabel = computed(() => userStore.alias || "You Know Who");
+const widgetIcon = computed(() => "i-ph-user-duotone");
 
 const items = computed(() => {
-  const menuItems = []
+  const menuItems = [];
   if (fluxStore.profile) {
     menuItems.push({
-      label: 'Profile',
-      icon: 'i-ph-user-circle-duotone',
+      label: widgetLabel,
+      icon: "i-ph-smiley-wink-duotone",
       to: `/profile/${fluxStore.profile.handle}`,
-    })
+    });
   }
   menuItems.push({
-    label: 'Sign Out',
-    icon: 'i-ph-sign-out',
+    label: "Sign Out",
+    icon: "i-ph-sign-out-duotone",
     click: async () => {
-      await authService.signOut()
-      fluxStore.clearProfile()
+      await authService.signOut();
+      fluxStore.clearProfile();
     },
-  })
-  return [menuItems]
-})
+  });
+  return [menuItems];
+});
 </script>
--- components/Layout/CompactHeader.vue
+++ components/Layout/CompactHeader.vue
@@ -1,14 +1,14 @@
 <template>
   <header
-    class="app-header fixed top-0 left-0 right-0 bg-heroic-uranium dark:bg-heroic-graphite border-b border-gray-200 z-10">
+    class="app-header fixed top-0 left-0 right-0 bg-heroic-uranium dark:bg-heroic-graphite border-b border-gray-200 z-10 min-w-80">
     <div class="max-w-7xl mx-auto mt-4 px-4 py-3 flex items-center relative">
       <NuxtImg
         src="/images/logo/logo_sq_brd_cherenkov_clear.png"
         alt="World of Nuclear Logo"
         class="w-12 h-12 -mt-4" />
-      <h1
-        class="absolute left-1/2 -translate-x-1/2 text-x3l font-bold text-[nuclear]">
-        Flux on World of Nuclear
+      <h1 class="absolute left-1/2 -translate-x-1/2 text-x3l font-bold">
+        <UIcon name="i-ph-lightning-duotone" /> Flux
+        <UIcon name="i-ph-lightning-duotone" />
       </h1>
       <AccountWidget class="-mt-3 ml-auto" />
     </div>
