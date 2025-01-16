4c8f5dcfb8861cee1bd85d503cd0465eec75b866
Zanzibar <zanzisworld42@gmail.com>
2025-01-15 17:05:20 -0800

    Navatar widget touch-ups

--- components/AccountWidget.vue
+++ components/AccountWidget.vue
@@ -5,42 +5,55 @@
     label="Sign In"
     to="/join"
     color="gray"
-    variant="solid" />
+    variant="solid"
+  />
   <UDropdown v-else :popper="{ placement: 'bottom-start' }" :items="items">
     <UButton
       color="primary"
       variant="solid"
       size="sm"
       :icon="widgetIcon"
-      trailing-icon="i-ph-caret-double-down" />
+      trailing-icon="i-ph-caret-double-down"
+    />
   </UDropdown>
 </template>
 
 <script lang="ts" setup>
-const userStore = useUserStore();
-const fluxStore = useFluxStore();
-const authService = useAuthService();
+const userStore = useUserStore()
+const fluxStore = useFluxStore()
+const authService = useAuthService()
 
-const widgetLabel = computed(() => userStore.alias || "You Know Who");
-const widgetIcon = computed(() => "i-ph-user-duotone");
+const widgetLabel = computed(() => userStore.alias || 'You Know Who')
+const widgetIcon = computed(() => 'i-ph-user-duotone')
 
 const items = computed(() => {
-  const menuItems = [];
+  const menuItems = []
+  menuItems.push({
+    label: widgetLabel,
+    labelClass: 'screen-name',
+  })
   if (fluxStore.profile) {
     menuItems.push({
-      label: widgetLabel,
-      icon: "i-ph-smiley-wink-duotone",
+      label: 'Profile',
+      icon: 'i-ph-smiley-wink-duotone',
       to: `/profile/${fluxStore.profile.handle}`,
-    });
+    })
   }
   menuItems.push({
-    label: "Sign Out",
-    icon: "i-ph-sign-out-duotone",
+    label: 'Sign Out',
+    icon: 'i-ph-sign-out-duotone',
     click: async () => {
-      await authService.signOut();
-      fluxStore.clearProfile();
+      await authService.signOut()
+      fluxStore.clearProfile()
     },
-  });
-  return [menuItems];
-});
+  })
+  return [menuItems]
+})
 </script>
+
+<style>
+.screen-name {
+  margin: 0 auto;
+  @apply text-heroic-cherenkov;
+}
+</style>
